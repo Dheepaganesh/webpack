8ad91729ad81ca6a6797c8aaf69d6afa55f9c84e
Dheepaganesh <dheepaganeshp3052dell@gmail.com>
2023-08-24 16:52:00 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import image from "../src/icons/react.png";
-import laugh from "../src/icons/laugh.svg";
+import image from "./icons/react.png";
+import laugh from "./icons/laugh.svg";
 
-const App = () => {
+function App() {
   return (
     <div>
       <h1>React App using Webpack and Yarn</h1>
@@ -12,6 +12,6 @@ const App = () => {
       <img src={laugh} alt="" />
     </div>
   );
-};
+}
 
 export default App;
